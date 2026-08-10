3b6584b332b74d4ae4cd18eaacdbc728445ce6f6
elyaginnings-cell <ely.a.ginnings@gmail.com>
2026-08-09 23:55:23 -0500

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,13 +1,11 @@
 export default function handler(req, res) {
-    const isProduction = process.env.NODE_ENV === "production";
-
     const cookieFlags = [
         "discord_session=",
         "Path=/",
         "HttpOnly",
         "Max-Age=0",
         "SameSite=Lax",
-        ...(isProduction ? ["Secure"] : [])
+        ...(process.env.NODE_ENV === "production" ? ["Secure"] : [])
     ].join("; ");
 
     res.setHeader("Set-Cookie", cookieFlags);
