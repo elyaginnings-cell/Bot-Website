32a56141c2ccde505077007b2b54873899ca4035
elyaginnings-cell <ely.a.ginnings@gmail.com>
2026-08-09 20:56:25 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Gatto Bot Dashboard loaded!");
+
+const loginButton =
+    document.querySelector(".discord-login");
+
+loginButton.addEventListener("click", () => {
+
+    alert(
+        "Discord login will be connected next!"
+    );
+
+});
