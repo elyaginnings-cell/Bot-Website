b3eb749a734f0071566f45fe418bbb419ddd751f
elyaginnings-cell <ely.a.ginnings@gmail.com>
2026-08-09 21:22:34 -0500

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,1 +1,24 @@
+const crypto = require("crypto");
 
+module.exports = async (req, res) => {
+    const state = crypto.randomBytes(32).toString("hex");
+
+    const params = new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        redirect_uri:
+            "https://bot-website-ruby-six.vercel.app/api/auth/callback",
+        response_type: "code",
+        scope: "identify guilds",
+        state
+    });
+
+    res.setHeader(
+        "Set-Cookie",
+        `oauth_state=${state}; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=600`
+    );
+
+    res.redirect(
+        302,
+        `https://discord.com/oauth2/authorize?${params.toString()}`
+    );
+};
