beb2ad5b30895bcc295348eebdb18f6d5ce99937
elyaginnings-cell <ely.a.ginnings@gmail.com>
2026-09-03 16:45:20 -0500

    Support state=link for attaching Discord to an email account

--- api/login.js
+++ api/login.js
@@ -6,12 +6,18 @@ export default function handler(req, res) {
     return res.status(500).send("Discord OAuth is not configured.");
   }
 
+  const state = req.query?.state === "link" ? "link" : "login";
+
   const params = new URLSearchParams({
     client_id: clientId,
     response_type: "code",
     redirect_uri: redirectUri,
     scope: "identify email guilds",
+    state,
   });
 
-  return res.redirect(302, `https://discord.com/oauth2/authorize?${params.toString()}`);
+  return res.redirect(
+    302,
+    `https://discord.com/oauth2/authorize?${params.toString()}`
+  );
 }
