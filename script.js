1547f24c2a9032b80a0eb366a5ca516e6db0d045
elyaginnings-cell <ely.a.ginnings@gmail.com>
2026-09-04 22:32:25 -0500

    restore: full dashboard script.js (Postgres save status)

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-(function(){
-  function load(src){return new Promise(function(res,rej){var s=document.createElement("script");s.src=src;s.onload=res;s.onerror=rej;document.head.appendChild(s);});}
-  load("script-part1.js?v=12").then(function(){return load("script-part2.js?v=12");}).catch(function(e){console.error(e);});
-})();
+SEE_FILE
