23422854ac52e350b6c46818a9279148c45a4caa
elyaginnings-cell <ely.a.ginnings@gmail.com>
2026-09-19 19:56:19 -0500

    Tickets v5: script parts loader + part 4

---
+++ tp4.js
@@ -0,0 +1,5 @@
+window.__tparts=window.__tparts||[];window.__tparts[4]=`owSection;var o=window.showSection;window.showSection=function(tab){var r=o.apply(this,arguments);if(String(tab)==="tickets")onTab();return r;};window.showSection.__tv5=true;return true;}
+[0,400,1000,2500,5000,9000].forEach(function(ms){setTimeout(function(){wrap();boot(true);},ms);});
+console.log("[features-tickets] v5 compact ready");
+})();
+`;
